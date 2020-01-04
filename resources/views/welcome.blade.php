dee20d8cbc0e9af984f655e7435a6084ab463d33
alvaro <alvaro.g.o96@gmail.com>
2020-01-04 16:45:01 +0100

    Comentarios del welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <h1>Ejemplo cliente TPV</h1>
-            <a class="nav-link" href="{{ url('/pago') }}">Página de pago</a>
+            <br>
+            <p>La página de pago es vuestra página para realizar el pago meidante el TPV.</p>
+            <p>El código de ejemplo para la pantilla se encuentra en /resources/views/pago.</p>
+            <p>El código de ejemplo para el controlador se encuentra en /Http/Controllers/TPVController.</p>
+            <p>La petición a nuestra api se puede hacer por formulario POST o por AJAX, en la plantilla se encuentran ambos ejemplos.</p>
+            <p>Para más información: <a href="https://github.com/alvarogo/ClienteTPV/blob/master/README.md">Readme</a></p>
+            <a  href="{{ url('/pago') }}">Página de pago</a>
 
         </div>
     </div>
